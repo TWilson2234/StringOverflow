ac2f7dc29a2788a3bdf3a54b89aaaf22aa784083
Yoshiminu <troy.waters@my.jcu.edu.au>
2015-05-29 13:30:34 +1000

    Updated About Us page
    I found multiple mistakes while reading the about us page, and decided
    to fix them.

--- aboutus.php
+++ aboutus.php
@@ -10,7 +10,7 @@
 
 <div class="sfeat">
 <h3>New To Townsville?</h3>
-	<p>Townsville has a population of about 200,000 and is growing at about 1 subrub per year, so there is a lot of musical activity. All private schools and most government schools have music teachers. The larger private schools are Townsville Grammar, the Cathedral School and Ryan Catholic College. The larger public hish schools are Pimlico and Kriwan.
+	<p>Townsville has a population of about 200,000 and is quickly growing at about 1 subrub per year, so there is bound to be a lot of musical activity. All private schools and most government schools in Australia have music teachers. The larger private schools are Townsville Grammar School, the Cathedral School and Ryan Catholic College. The large public high schools in Townsville are Pimlico State High School and Kirwan Ste High School.
     <h3>More Information</h3>
     <a href="http://www.townsville.qld.gov.au/townsville/infocentre/Pages/default.aspx">Townsville Info Center</a>
 </div>
@@ -28,44 +28,44 @@
 
 <div class="sfeat">
 <h3>Busking</h3>
-<p>Busking is permitted at several public spaces around the city with a Buskers Permit from the city council.<br><br>There is no age limit, but buskers 16 and under will need to be accompanied by a parent/gaurdian.</p>
+<p>Busking is permitted at several public spaces around the city with a Buskers Permit from the city council.<br><br>There is no age limit to busking, but buskers 16 and under will need to be accompanied by a parent or appropraite gaurdian.</p>
 <p>Phone: 4727 9680</p>
 </div>
 
 <div class="sfeat">
 <h3>Sponsors</h3>
-<p>OUr music concerts are well-publicised and well-attended and sponsors enjoy brand exposure throughout the Arts community. We welcome sponsoship or assistance in any form.<p>
+<p>Our music concerts are well-publicised and attended by many people and sponsors always enjoy the brand exposure throughout the Arts community. We welcome sponsoship or assistance in any form.<p>
 <h3>Out Current Sponsors:</h3>
 <p><strong>Townsville City Council</strong><p>
-<p><img src="sources/city of townsville.png">The Council's partnership and sponsorships sceme provides vital core funding which enables us to maintain the administrative base for all our other activites, and also provides the premisise which house our office space. The council also assists with the performance cenues for our concerts and workshops.</p>
+<p><img src="sources/city of townsville.png">The Townsville City Council's partnership and sponsorships scheme provides vital core funding which enables us to maintain the administrative base for all our activites, and also provides the premises in which our office space is located. The council also assists with the performance venues for our concerts and workshops.</p>
 <p><strong>Queendsland Government</strong></p>
-<p><img src="sources/govemblem.png">The Gambling Community Benefit Fund has assisted us to obtain office equipment, sound and lighting equipment for our prodctions.</p>
+<p><img src="sources/govemblem.png">The Gambling Community Benefit Fund has assisted us greatly by obtaining office equipment, sound and lighting equipment for our prodctions.</p>
 </div>
 
 
-<h2>Townsville Community Music Center</h2>
-<p>Based in Townsville, north Queendsland, the Music Centre presents concerts and workshops throughout the year, in a diverse range of genrres including calssical, jazz, folk, blues, world and contemporary music with touring artits, locally-based professionals and emerging artists.</p>
+<h2>Townsville Community Music Centre</h2>
+<p>Based in Townsville at North Queensland, the Townsville Community Music Centre presents concerts and workshops throughout the entire year, in a diverse range of genrres including classical, jazz, folk, blues, world and contemporary music with countless touring artits, locally-based professionals and emerging artists.</p>
 
 <h2>30th Anniversary in 2013</h2>
 <img src="sources/old-music.jpg">
-<p>It has been constantly changing over the years to keep up to date with the musical tastes and needs of the Townsville community. As part of the relocation of the Music Centre to the Civic Theatre, Bronia Renison and Jean Dartnall, both librarians, have assessed the old collection of sheet music, books and recorded music which the centre has been storing, unused, for many years. Sometimes older things have to be discarded to make way for the new, but the Music Centre is aware that older material may still have value. <br>
-The National Library of Australia has an online catalogue (TROVE) that lists not only its own holdings but also information about items held by many other libraries around Australia. Using this catalogue Bronia and Jean have identified at least 150 items of music that are not held by any of the country's major libraries. These items have been donated to the National Library to include in their collection and thus made available to all historians and musicians.<br><br>
+<p>The Townsville Community Music Centre has been constantly changing over the years in order to keep up to date with the musical tastes and needs of the Townsville community. As part of the relocation of the Music Centre to the Civic Theatre, Bronia Renison and Jean Dartnall, both librarians, have assessed the old collection of sheet music, books and recorded music which the centre has been storing, unused, for many years. Sometimes older things have to be discarded to make way for the new, but the Music Centre is aware that even older material may still have value. <br>
+The National Library of Australia has an online catalogue (TROVE) that lists not only its own holdings but also information about items held by many other libraries around Australia. Using this catalogue Bronia and Jean have identified at least 150 items of music that are not held by any of the country's other major libraries. These items have been donated to the National Library to include in their collection and thus made available to all interested historians and musicians.<br><br>
 
-Also discovered in the old collection were some pieces relevant to North Queensland. Local musicians performed these at a musical social afternoon on Sunday April 21st in C2 at the Civic Theatre. The remaining sheet music, books and CDs were put on display and distributed free of charge to the local music community.</p>
+Also discovered in the old collection were some pieces relevant to North Queensland. Local musicians performed these at a musical social on Sunday April 21st in C2 at the Civic Theatre. The remaining sheet music, books and CDs were put on display and distributed free of charge to the local music community.</p>
 
 <h2>A Breif History by Jean Dartnall</h2>
 <ul>
-<li><strong>1983</strong> - The Townsville Community Music Centre was established at a public meeting on May 24th. Then Deputy Mayor, Ken McElligott, opened the meeting and presented a cheque for $50 from the City Council to cover initial petty cash, the Centre's first funds. At the meeting an executive committee was formed. Over the next few weeks that committee drafted a constitution which was endorsed at another public meeting on June 15th. Fred Thompson was Chairman from September 1983, with Gordon Dean as his Deputy and Jan Eagleton as Secretary.</li>
-<li><strong>1984</strong> - Mira Henderson, who had extensive experience in community music work in England, was the first staff member. (Her position was Acting Director because she was only in Townsville for a brief spell.) She instigated a series of classes starting in 1984 using the skills of people living in Townsville to enhance and encourage music learners of all ages.</li>
-<li><strong>1985</strong> -	Kirsty Veron was appointed as the first Director. Kirsty was very interested in teaching music to children and had worked with Mira in the 1983 classes. She held this position until 1988 leading the new organisation into a strong position in the Townsville arts community.</li>
-<li><strong>1985</strong> -	The Music Centre produced Benjamin Britten's Noyes Fludde in St James Anglican Cathedral. This involved hundreds of children and some of Townsville's best known (and loved) musicians. It was produced by Rachel Berker (now Rachel Matthews) and involved Bernie Lannigan and Sandra Voss in the main parts rehearsed by Bill Williams; Donna McMahon and Jenny Carr rehearsing the strings and recorders and guest conductor Donald Hollier. This was by the no means the only major production mounted by the Music Centre but was probably the largest.</li>
-<li><strong>1987</strong> -	This was the year of the first fund raising dinner. These dinners became a tradition as they were a wonderful mixture of good food, good wine and good music. The first ones were held at the home of Paul and June Tonnoir in North Ward. Later they moved to the Bishop's Lodge as guests of the Anglican Bishop. Attendees would often book their tickets for the next year's dinner as soon as they had attended this one. They ran until 2004.</li>
-<li><strong>1988</strong> -	Mary Lou Schoenfeldt was appointed as Administrator. Although this was initially a small part time job with limited responsibilities, Mary Lou made the job, the Centre and indeed music in Townsville, her own. She was a wonderful asset to the Centre and to the cultural life of the City until she had to retire due to ill health in 2003.</li>
+<li><strong>1983</strong> - The Townsville Community Music Centre was established at a public meeting on May 24th. The Deputy Mayor at the time, Ken McElligott, opened the meeting and presented a cheque for $50 from the City Council to cover initial petty cash, the Centre's first ever funds. At the meeting an executive committee was formed. Over the next few weeks that committee drafted a constitution which was endorsed at another public meeting on June 15th that year. Fred Thompson was elected Chairman from September 1983, with Gordon Dean as his Deputy and Jan Eagleton as Secretary.</li>
+<li><strong>1984</strong> - Mira Henderson, who had extensive experience in community music work in England, was the first staff member. (Her position was Acting Director because she was only in Townsville for a brief period.) She instigated a series of classes starting in 1984 using the skills of people living in Townsville to enhance and encourage new music learners of all ages.</li>
+<li><strong>1985</strong> -	Kirsty Veron was appointed as the first Director. Kirsty was very interested in teaching music to children and had worked with Mira in the 1983 classes. She held this position until 1988, leading the new organisation into a strong position in the Townsville arts community.</li>
+<li><strong>1985</strong> -	The Music Centre produced Benjamin Britten's "Noyes Fludde" in the St James Anglican Cathedral. This involved hundreds of children and some of Townsville's best known (and loved) musicians. It was produced by Rachel Berker (now Rachel Matthews) and involved Bernie Lannigan and Sandra Voss in the main parts rehearsed by Bill Williams; Donna McMahon and Jenny Carr rehearsing the strings and recorders and guest conductor Donald Hollier. This was by the no means the only major production mounted by the Music Centre but this was probably the largest.</li>
+<li><strong>1987</strong> -	This was the year of the first fund raising dinner. These dinners have since became a tradition as they were a wonderful mixture of good food, good wine and good music. The first ones were held at the home of Paul and June Tonnoir in North Ward. Later they moved to the Bishop's Lodge as guests of the Anglican Bishop. Attendees would often book their tickets for the next year's dinner as soon as they had attended the previous one. These dinners ran until 2004.</li>
+<li><strong>1988</strong> -	Mary Lou Schoenfeldt was appointed as Administrator. Although this was initially a small part time job with limited responsibilities, Mary Lou made the job, the Centre and indeed music in Townsville, her own. She was a wonderful asset to the Centre and to the cultural life of the City until she had to retire due to poor health in 2003.</li>
 <li><strong>1990</strong> - This year the first lunchtime concert was held at the PercTucker Gallery. The importance of providing performance opportunities for emerging musicians had become evident and from this time, for a number of years, one of the major activities of the Centre was the production and management of concerts.</li>
 <li><strong>1999</strong> -	Arts Queensland had been a major funder of Music Centre activities for a number of years but in this year they agree to provide funding on a three year basis. This made it much easier for the Centre to plan ahead and reduced the time that had to be spent on repetitive grant applications. This arrangement ran until 2004.</li>
 <li><strong>2003</strong> -	A spectacular variety concert was held at the Townsville Civic Theatre to celebrate twenty years of the Music Centre and as a tribute to May Lou.</li>
-<li><strong>2005</strong> -	The Centre moved to offices in the Old Magistrate's Court.</li>
-<li><strong>2012</strong> -	The Centre moved to its present office in the Townsville Civic Theatre building.</p></li>
+<li><strong>2005</strong> -	The Centre moved into new offices in the Old Magistrate's Court.</li>
+<li><strong>2012</strong> -	The Centre moved into its present office in the Townsville Civic Theatre building.</p></li>
 </ul>
 <p>More details of the period 1983 - 2008 can be found in the book<br><strong>"Townsville Community Music Centre: Some Memories of the first 25 years"</strong><br>by Jean Dartnall. (Coppies avaliable from the music center)</p>
 
